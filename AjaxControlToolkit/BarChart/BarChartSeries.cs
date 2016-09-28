cdb92eb4dd9d8719b6bf4c6a8d5b3cccd45ae68b
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-09-28 15:49:25 +0300

    add BarChartSeries documentation

--- AjaxControlToolkit/BarChart/BarChartSeries.cs
+++ AjaxControlToolkit/BarChart/BarChartSeries.cs
@@ -10,16 +10,31 @@ namespace AjaxControlToolkit {
         string _name = String.Empty;
         string _barColor = string.Empty;
 
+        /// <summary>
+        /// A name of the series.
+        /// </summary>
+        /// <remarks>
+        /// This property is required.
+        /// </remarks>
         public string Name {
             get { return _name; }
             set { _name = value; }
         }
 
+        /// <summary>
+        /// Sets the color of bar for a particular series.
+        /// </summary>
         public string BarColor {
             get { return _barColor; }
             set { _barColor = value; }
         }
 
+        /// <summary>
+        /// Provides data for a particular series.
+        /// </summary>
+        /// <remarks>
+        /// This property is required.
+        /// </remarks>
         [TypeConverter(typeof(DataConverter<decimal>))]
         public decimal[] Data { get; set; }
     }
