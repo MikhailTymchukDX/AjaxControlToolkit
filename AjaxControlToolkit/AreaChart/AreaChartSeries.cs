dc4223dd17db98e6bec80e5c5110904b4cfd81c2
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-09-29 11:40:23 +0300

    add AreaChartSeries docs

--- AjaxControlToolkit/AreaChart/AreaChartSeries.cs
+++ AjaxControlToolkit/AreaChart/AreaChartSeries.cs
@@ -9,16 +9,31 @@ namespace AjaxControlToolkit {
         string _name = String.Empty;
         string _areaColor = String.Empty;
 
+        /// <summary>
+        /// A name of the series.
+        /// </summary>
+        /// <remarks>
+        /// This property is required.
+        /// </remarks>
         public string Name {
             get { return _name; }
             set { _name = value; }
         }
 
+        /// <summary>
+        /// Sets the color of area for a particular series.
+        /// </summary>
         public string AreaColor {
             get { return _areaColor; }
             set { _areaColor = value; }
         }
 
+        /// <summary>
+        /// Provides data for a particular series.
+        /// </summary>
+        /// <remarks>
+        /// This property is required.
+        /// </remarks>
         [TypeConverter(typeof(DataConverter<decimal>))]
         public decimal[] Data { get; set; }
     }
