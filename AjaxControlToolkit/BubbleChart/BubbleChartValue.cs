a86c039cbe79be140d5774286815a9b4194a3f23
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2016-09-29 13:25:16 +0300

    add BubbleChartValue docs

--- AjaxControlToolkit/BubbleChart/BubbleChartValue.cs
+++ AjaxControlToolkit/BubbleChart/BubbleChartValue.cs
@@ -12,26 +12,53 @@ namespace AjaxControlToolkit {
         decimal _data;
         string _bubbleColor = String.Empty;
 
+        /// <summary>
+        /// A name of the category.
+        /// </summary>
+        /// <remarks>
+        /// This property is required.
+        /// </remarks>
         public string Category {
             get { return _category; }
             set { _category = value; }
         }
 
+        /// <summary>
+        /// Provides X value for a particular BubbleChartValue.
+        /// </summary>
+        /// <remarks>
+        /// This property is required.
+        /// </remarks>
         public decimal X {
             get { return _x; }
             set { _x = value; }
         }
 
+        /// <summary>
+        /// Provides Y value for a particular BubbleChartValue.
+        /// </summary>
+        /// <remarks>
+        /// This property is required.
+        /// </remarks>
         public decimal Y {
             get { return _y; }
             set { _y = value; }
         }
 
+        /// <summary>
+        /// Provides Data value for a particular BubbleChartValue.
+        /// </summary>
+        /// <remarks>
+        /// This property is required.
+        /// </remarks>
         public decimal Data {
             get { return _data; }
             set { _data = value; }
         }
 
+        /// <summary>
+        /// Sets the color of the bubble for a particular BubbleChartValue.
+        /// </summary>
         public string BubbleColor {
             get { return _bubbleColor; }
             set { _bubbleColor = value; }
